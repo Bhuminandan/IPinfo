28bc9db82fc87f105f44de3a2efd7bd580d2cf0e
Bhumi <bhumione9545@gmail.com>
2023-08-19 19:48:31 +0530

    Adding for the first time

---
+++ script.js
@@ -0,0 +1,22 @@
+let homePageIPHeading = document.querySelector(".right-heading span");
+let getStartedBtn = document.querySelector(".get-started")
+
+
+
+
+// finding the IP Address
+// $.getJSON("https://api.ipify.org?format=json", function (data) {
+//     printIPHomePage(data.ip);
+// })
+
+// Manupalating the IP Adress Heading of Home Page
+function printIPHomePage(ip) {
+    homePageIPHeading.innerHTML = `${ip}`;
+    localStorage.setItem("ipAddress", ip)
+}
+
+// Adding functionality to Get Started
+getStartedBtn.addEventListener("click", () => {
+    let url = "/details.html"
+    window.location.href = url;
+})
